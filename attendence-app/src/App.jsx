8a11e42625f7b4f5bf030eb4e860c0b2fd590d17
Nishant Singh <rajhemant7243@gmail.com>
2025-10-26 21:44:56 +0530

    Update App.jsx

--- attendence-app/src/App.jsx
+++ attendence-app/src/App.jsx
@@ -4,27 +4,20 @@ import Login from './components/Login';
 import Register from './components/Register';
 import Dashboard from './components/Dashboard';
 import AttendanceHistory from './components/AttendanceHistory';
+import SubjectManager from './components/SubjectManager';
 import ProtectedRoute from './components/ProtectedRoute';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />  {/* Home added */}
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        
-        <Route path="/dashboard" element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        } />
-        
-        <Route path="/history" element={
-          <ProtectedRoute>
-            <AttendanceHistory />
-          </ProtectedRoute>
-        } />
+
+        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        <Route path="/history" element={<ProtectedRoute><AttendanceHistory /></ProtectedRoute>} />
+        <Route path="/subjects" element={<ProtectedRoute><SubjectManager /></ProtectedRoute>} />
       </Routes>
     </BrowserRouter>
   );
