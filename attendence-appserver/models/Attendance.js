dcf7da5a690239cc5c3ce869cb0585cb0f246893
Nishant Singh <rajhemant7243@gmail.com>
2025-10-26 21:49:15 +0530

    Update Attendance.js

--- attendence-appserver/models/Attendance.js
+++ attendence-appserver/models/Attendance.js
@@ -2,8 +2,10 @@ const mongoose = require("mongoose");
 
 const attendanceSchema = new mongoose.Schema({
     student: { type: mongoose.Schema.Types.ObjectId, ref: "Student", required: true },
-    date: { type: String, required: true },
-    status: { type: String, default: "present" },
-});
+    subject: { type: mongoose.Schema.Types.ObjectId, ref: "Subject", required: true },
+    type: { type: String, enum: ["Class", "Lab"], default: "Class" },
+    date: { type: Date, required: true },
+    status: { type: String, enum: ["Present", "Absent"], default: "Absent" },
+}, { timestamps: true });
 
 module.exports = mongoose.model("Attendance", attendanceSchema);
